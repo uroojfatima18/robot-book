d7a045fd582a6cc8aad0da3d538969a2ae0a5361
Urooj Fatima <uroojf199@gmail.com>
2026-02-17 01:14:41 +0500

    Configure Docusaurus for GitHub Pages deployment

--- my-website/docusaurus.config.js
+++ my-website/docusaurus.config.js
@@ -13,15 +13,16 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-website.com',
+  url: 'https://uroojfatima18.github.io',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/robot-book/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'uroojfatima18', // Usually your GitHub org/user name.
+  projectName: 'robot-book', // Usually your repo name.
+  trailingSlash: false,
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
